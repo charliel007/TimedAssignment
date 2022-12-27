1f8fe9eb87ca29961142229f4fb74407d8cf757b
Charlie Lipperd <lipperd@hotmail.com>
2022-12-27 17:31:55 -0500

    added sqldatabase

--- TimedAssignment.Data/Entities/CommentEntity.cs
+++ TimedAssignment.Data/Entities/CommentEntity.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace TimedAssignment.Models.Entities
 {
     public class CommentEntity
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string Text { get; set; }
         public Guid AuthorId { get; set; }
     }
