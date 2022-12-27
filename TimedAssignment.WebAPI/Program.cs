8c36b69597086e8cdf961087bdce588eea66649d
Charlie Lipperd <lipperd@hotmail.com>
2022-12-27 13:22:33 -0500

    Group walkthrough

--- TimedAssignment.WebAPI/Program.cs
+++ TimedAssignment.WebAPI/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TimedAssignment.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
